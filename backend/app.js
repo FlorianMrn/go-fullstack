3cbc29bc03ed9bffe11820936cc8296f207edc7d
Florian Merrien <fmerrien.dev@gmail.com>
2020-04-15 17:13:47 +0200

    Post Request added

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,27 @@
-// Import Express
+// Import
 const express = require('express');
+const bodyParser = require('body-parser');
 
 // App
 const app = express();
 
     // Middlewares
+    app.use(bodyParser.json());
+
     app.use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
         res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
         next();
       });
-      
+    
+    app.post('/api/stuff', (req, res, next) => {
+        console.log(req.body);
+        res.status(201).json({
+          message: 'Objet créé !'
+        });
+      });
+
     app.use('/api/stuff', (req, res, next) => {
         const stuff = [
           {
